823712159d8397d1852e51c25dd4cec3c8701a80
Harry <77958121+HardikJain-Harry@users.noreply.github.com>
2022-02-05 23:24:47 -0800

    Time: 7 ms (60.40%), Space: 11 MB (9.46%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -3,29 +3,12 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
         if(n < 3) return n;
-        vector<int> res;
-        int i = 0,j = 1;
-        while(j < n) {
-            if(nums[j] == nums[j - 1]) {
-                j++;
-            }else{
-                if(j - i >= 2) {
-                    res.push_back(nums[j - 1]);
-                }
-                res.push_back(nums[j - 1]);
-                i = j;
-                j++;
+        int ln = 2;
+        for(int i = 2; i < n; i++) {
+            if(nums[ln - 2] != nums[i]){
+                nums[ln++] = nums[i];
             }
         }
-        int c = 2;
-        while(i < j && c){
-            res.push_back(nums[i++]);
-            c--;
-        }
-            
-        for(int idx = 0; idx < res.size(); idx++) {
-            nums[idx] = res[idx];
-        }
-        return res.size();
+        return ln;
     }
 };
