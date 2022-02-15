385effa13f3aef29cfc9133a4f43d08443be2ad3
Harry <77958121+HardikJain-Harry@users.noreply.github.com>
2022-02-15 10:13:21 -0800

    Time: 15 ms (38.23%), Space: 10.1 MB (43.33%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> res;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if((i == 0 || nums[i - 1] != nums[i]) &&
+               (i == n - 1 || nums[i + 1] != nums[i])){
+                   res.push_back(nums[i]);
+               }
+        }
+        return res;
+    }
+};
