f6f0c6cdb23731c490d8e9ada65f8cb0752eeba9
Harry <77958121+HardikJain-Harry@users.noreply.github.com>
2022-02-21 09:34:46 -0800

    Time: 0 ms (100.00%), Space: 5.8 MB (75.67%) - LeetHub

--- 319-bulb-switcher/319-bulb-switcher.cpp
+++ 319-bulb-switcher/319-bulb-switcher.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        int res = 0;
-        for(int i = 1; i * i <= n; i++) {
-            res++;
-        }
-        return res;
+        return sqrt(n);
     }
 };
