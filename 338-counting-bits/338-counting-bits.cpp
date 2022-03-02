4120dbeb0d29c7609a233b2ecd23cb99e806c813
Harry <77958121+HardikJain-Harry@users.noreply.github.com>
2022-03-02 09:54:16 -0800

    Time: 16 ms (10.61%), Space: 7.9 MB (74.98%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int cnt(int n){
-        int c = 0;
-        while(n){
-            n &= (n-1);
-            c++;
-        }
-        return c;
+    int cnt(int n, vector<int>& memo){
+        if(n == 1) return 1;
+        if(n == 0) return 0;
+        if(memo[n] != 0) return memo[n];
+        if(n % 2 == 0) return memo[n] = cnt(n/2,memo);
+        else return memo[n] = 1 + cnt(n/2,memo);
     }
     vector<int> countBits(int n) {
-        vector<int> ans;
+        vector<int> ans(n+1,0);
         for(int i = 0; i <= n; i++){
-            ans.push_back(cnt(i));
+            ans[i] = cnt(i,ans);
         }
         return ans;
     }
