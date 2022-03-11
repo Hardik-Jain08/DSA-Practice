5ca13fd6c4820b1c9f61b6b0224bf4288d3dde13
Harry <77958121+HardikJain-Harry@users.noreply.github.com>
2022-03-12 00:24:52 +0530

    Time: 7 ms (45.13%), Space: 11.1 MB (30.47%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-    int pivot(vector<int>& nums){
+    int search(vector<int>& nums, int target) {
         int s = 0, e = nums.size() - 1,m;
         while(s <= e){
             m = s + (e - s) / 2;
-            if(m < e && nums[m] > nums[m + 1]){
-                return m;
+            if(nums[m] == target) return m;
+            else if(nums[s] <= nums[m]){
+                if(target <= nums[m] && target >= nums[s]){
+                    e = m - 1;
+                }else{
+                    s = m + 1;
+                }
+            }else{
+                if(target <= nums[e] && target >= nums[m]){
+                    s = m + 1;
+                }else{
+                    e = m - 1;
+                }
             }
-            if(m > s && nums[m] < nums[m - 1]){
-                return m - 1;
-            }
-            if(nums[s] >= nums[m]){
-                e = m - 1;
-            }else {
-                s = m + 1;
-            }
-        }
-        return s;
-    }
-    int se(vector<int>& nums,int s, int e, int target) {
-        int left = s, right = e, mid;
-        while(left <= right){
-            mid = left + (right - left) / 2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
         }
         return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        int p = pivot(nums);
-        int ans = se(nums, 0, p, target);
-        if(ans == -1) ans = se(nums, p + 1, nums.size() - 1, target);
-        return ans;
     }
 };
