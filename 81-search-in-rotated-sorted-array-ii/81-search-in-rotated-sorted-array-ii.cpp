8cabdb15d6721d9508672e600a660f4f2be00189
Harry <77958121+HardikJain-Harry@users.noreply.github.com>
2022-03-28 22:09:32 +0530

    Time: 11 ms (41.14%), Space: 13.8 MB (90.12%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -5,18 +5,18 @@ public:
         while(s <= e){
             m = s + (e - s) / 2;
             if(nums[m] == target) return true;
-            else if(nums[s] == nums[m] && nums[e] == nums[m]) s++,e--;
-            else if(nums[s] <= nums[m]){
-                if(target <= nums[m] && target >= nums[s]){
-                    e = m - 1;
-                }else{
+            else if(nums[s] == nums[m] && nums[e] == nums[m]) s++, e--;
+            else if(nums[m] < nums[s]){
+                if(nums[m] < target && target <= nums[e]){
                     s = m + 1;
+                }else{
+                    e = m - 1;
                 }
             }else{
-                if(target >= nums[m] && target <= nums[e]){
-                    s = m + 1;
-                }else{
+                if(nums[m] > target && target >= nums[s]){
                     e = m - 1;
+                }else{
+                    s = m + 1;
                 }
             }
         }
