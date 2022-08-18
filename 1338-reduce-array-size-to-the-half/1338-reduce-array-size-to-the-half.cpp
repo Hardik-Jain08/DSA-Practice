273e84f4cb329c7e9efa027280e0314c8225136a
Hardik Jain <77958121+HardikJain-Harry@users.noreply.github.com>
2022-08-18 13:04:47 -0700

    Time: 291 ms (68.64%), Space: 78.3 MB (63.73%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    static bool compare(pair<int,int> &p1, pair<int, int> &p2){
-        if (p1.second == p2.second)
-            return p1.first < p2.first;
-        return p1.second > p2.second;
-    }
-    
     int minSetSize(vector<int>& arr) {
-        map<int, int> freq;
+        unordered_map<int, int> freq;
         for(auto x : arr) freq[x]++;
         int s = freq.size();
-        pair<int, int> p[s];
+        vector<int> fmap;
         int i = 0;
         for (auto it = freq.begin(); it != freq.end(); ++it)
-            p[i++] = {it->first, it->second};
+            fmap.push_back(it->second);
  
-        sort(p, p + s, compare);
+        sort(fmap.begin(), fmap.end(), greater<int>());
         int n = arr.size() / 2, ans = 0, cnt = 0;
-        i = 0;
         while(n > ans){
-            ans += p[i++].second, cnt++;
+            ans += fmap[i++], cnt++;
         }
         return cnt;
     }
