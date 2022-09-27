f85e518362e9c18c06ce5ec2a26c641387df851e
Hardik Jain <77958121+HardikJain-Harry@users.noreply.github.com>
2022-09-26 22:29:21 -0700

    Time: 118 ms (14.98%), Space: 132.4 MB (5.77%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -3,14 +3,21 @@ public:
     string removeDuplicates(string s, int k) {
         stack<pair<char, int>> st;
         for(int i = 0; i < s.size(); i++){
-            if(st.empty() || st.top().first != s[i]){
-                st.push({s[i], 1});
-            }else {
-                st.top().second++;
+            if(st.empty() || st.top().first!=s[i])
+            {
+                st.push({s[i],1});                
             }
-            if(!st.empty() && st.top().second >= k){
-                st.top().second -= k;;
-                if(st.top().second == 0) st.pop();
+            else if(st.top().first==s[i])
+            {
+                if(st.top().second < k-1)
+                {
+                    st.top().second++;
+                }
+                else
+                {
+                    st.pop();
+                }
+                
             }
         }
         string ans = "";
