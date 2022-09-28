1c4ddc35556cf08c8d7d4af365d777e1fec03e84
Hardik Jain <77958121+HardikJain-Harry@users.noreply.github.com>
2022-09-28 05:32:14 -0700

    Time: 90 ms (5.10%), Space: 8.6 MB (76.32%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> umap;
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        for(int i = 0; i < n1; i++){
-            umap[nums1[i]] = i;
-        }
-        vector<int> res(n1,0);
         stack<int> st;
-        for(int i = n2 - 1; i >= 0; i--){
-            while(!st.empty() && st.top() < nums2[i]){
-                st.pop();
-            }
-            if(umap.find(nums2[i]) != umap.end()){
+        vector<int> nge(nums2.size());
+        for(int i = nums2.size() - 1; i >= 0; i--){
+            if(st.empty()){
+                nge[i] = -1;
+            }else if(st.top() > nums2[i]){
+                nge[i] = st.top();
+            }else{
+                while(!st.empty() && st.top() <= nums2[i]){
+                    st.pop();
+                }
                 if(st.empty()){
-                    res[umap[nums2[i]]] = -1;
+                    nge[i] = -1;
                 }else{
-                    res[umap[nums2[i]]] = st.top();
+                    nge[i] = st.top();
                 }
             }
             st.push(nums2[i]);
+            cout << nge[i] << " ";
         }
-        return res;
+        vector<int> ans(nums1.size());
+        for(int i = 0; i < nums1.size(); i++){
+            for(int j = 0; j < nums2.size(); j++){
+                if(nums1[i] == nums2[j]){
+                    ans[i] = nge[j];
+                }
+            }
+        }
+        return ans;
     }
 };
