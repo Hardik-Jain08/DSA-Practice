d380942b49aef2cdba8e075c135aede6c9013d64
Hardik Jain <77958121+HardikJain-Harry@users.noreply.github.com>
2022-10-02 12:12:29 -0700

    Time: 220 ms (83.28%), Space: 81.3 MB (51.02%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,54 +1,30 @@
-class Solution
-{
-    public:
-        int largestRectangleArea(vector<int> &arr)
-        {
-            int n = arr.size();
-            int ans = 0;
-            vector<int> left(n, 0);
-            stack<int> st_l;
-            
-            for (int i = 0; i < n; i++)
-            {
-                while (!st_l.empty() && arr[st_l.top()] >= arr[i])
-                {
-                    st_l.pop();
-                }
-                if (st_l.empty())
-                {
-                    left[i] = -1;
-                }
-                else
-                {
-                    left[i] = st_l.top();
-                }
-                st_l.push(i);
-            }
-
-            vector<int> right(n, 0);
-            stack<int> st_r;
-            
-            for (int i = n - 1; i >= 0; i--)
-            {
-                while (!st_r.empty() && arr[st_r.top()] >= arr[i])
-                {
-                    st_r.pop();
-                }
-                if (st_r.empty())
-                {
-                    right[i] = n;
-                }
-                else
-                {
-                    right[i] = st_r.top();
-                }
-                st_r.push(i);
-            }
-            for (int i = 0; i < n; i++)
-            {
-               	// cout << left[i] << " " << right[i] << endl;
-                ans = max(ans, (right[i] - left[i] - 1) *arr[i]);
-            }
-            return ans;
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& h) {
+        int n = h.size();
+        vector<pair<int,int>> nse(n);
+        stack<int> st;
+        // next smaller element to the right
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.empty() && h[st.top()] >= h[i]) st.pop();
+            if(st.empty()) nse[i].first = n;
+            else nse[i].first = st.top();
+            st.push(i);
         }
+        // empty the stack if there is something left 
+        while(!st.empty()) st.pop();
+        // next smaller element to the left
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && h[st.top()] >= h[i]) st.pop();
+            if(st.empty()) nse[i].second = -1;
+            else nse[i].second = st.top();
+            st.push(i);
+        }
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            ans = max(ans,max(h[i], (nse[i].first - nse[i].second - 1)*h[i]));
+        }
+        return ans;
+    }
 };
