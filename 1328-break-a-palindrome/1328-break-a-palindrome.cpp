27ca4ce7efeeb982fd2ba4e8a4b7e575bf385f2a
Hardik Jain <77958121+HardikJain-Harry@users.noreply.github.com>
2022-10-10 19:15:55 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.51%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     string breakPalindrome(string str) {
         if(str.size() == 1) return "";
-        for(int i = 0; i < str.size(); i++){
-            if(str[i] != 'a' && (i != (str.size()/2))){
+        for(int i = 0; i < str.size()/2; i++){
+            if(str[i] != 'a'){
                 str[i] = 'a';
                 return str;
             }
