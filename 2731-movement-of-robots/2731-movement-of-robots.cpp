f1f69a2c607ca2794f790b1ae6f019a4ee78e2ab
Hardik Jain <77958121+HardikJain-Harry@users.noreply.github.com>
2023-06-12 08:14:42 -0700

    Time: 144 ms (66.67%), Space: 93.2 MB (66.67%) - LeetHub

--- 2731-movement-of-robots/2731-movement-of-robots.cpp
+++ 2731-movement-of-robots/2731-movement-of-robots.cpp
@@ -8,15 +8,11 @@ public:
             nums[i] += (s[i] == 'L') ? -d : d;
         }
         sort(nums.begin(), nums.end());
-        vector<ll> pf(n,0);
-        pf[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            pf[i] = pf[i-1] + nums[i];
-        }
-        ll ans = 0;
+        ll ans = 0, pf = nums[0];
         for(ll i = 1; i < n; i++) {
-            ll t = (M+ i*nums[i] - pf[i-1])%M;
-            ans = (ans + t)%M;
+            ans += i*nums[i] - pf;
+            ans %= M;
+            pf += nums[i];
         }
         return (int)ans;
     }
